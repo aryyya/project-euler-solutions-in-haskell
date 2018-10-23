e5a95f892f007673a9f77dae6bbb34055d18c897
aryyya <arya@live.ca>
2018-10-23 12:21:00 -0700

    Add solution to question 4.

---
+++ 004-largest-palindrome-product.hs
@@ -0,0 +1,22 @@
+main :: IO ()
+main = do
+  putStrLn $ show $ solution
+
+solution :: Int
+solution = maximum [z | x <- range, y <- range, let z = x * y, pal $ digits z]
+
+range :: [Int]
+range = [100..999]
+
+pal :: [Int] -> Bool
+pal [] = True
+pal ns = head ns == last ns && pal (inner ns)
+
+digits :: Int -> [Int]
+digits 0 = []
+digits n = digits (n `div` 10) ++ [n `mod` 10]
+
+inner :: [a] -> [a]
+inner []  = []
+inner [x] = []
+inner xs  = tail $ take (length xs - 1) xs
